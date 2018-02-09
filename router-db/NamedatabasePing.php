15c27e92267a3290f818ebee439f1863bf5bf9f8
avantis <uaruscom@gmail.com>
2018-02-09 22:59:44 +0200

    Update and rename DatabasenamePing.php to NamedatabasePing.php

---
+++ router-db/NamedatabasePing.php
@@ -0,0 +1,39 @@
+<?php
+ 
+namespace YourPackage\Namedatabase;
+ 
+use Pllano\RouterDb\Ex;
+ 
+class NamedatabasePing
+{
+ 
+    private $config;
+ 
+    public function __construct(array $config = array())
+    {
+        if (count($config) >= 1){
+            $this->config = $config;
+        }
+    }
+ 
+    public function ping($resource = null)
+    {
+        if ($resource != null) {
+            try {
+                // Здесь должен быть код для проверки доступности таблицы (ресурса) в базе данных
+                // Должен возвращать название базы namedatabase или null если база недоступна
+                
+                // Этот код работает без проверки доступности базы
+                $response = $resource;
+ 
+                return $response;
+ 
+            } catch (Ex $ex) {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+ 
+}
