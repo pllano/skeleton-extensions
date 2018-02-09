4551cd0b3115bda3397849a6aea3353640a87834
avantis <uaruscom@gmail.com>
2018-02-09 23:02:36 +0200

    Update and rename JsonCache.php to NameCache.php

---
+++ cache/NameCache.php
@@ -0,0 +1,43 @@
+<?php
+ 
+namespace Pllano\Caching\Cache;
+ 
+class NameCache {
+ 
+    private $data;
+ 
+    public function __construct($data)
+    {
+        $this->$data = $data;
+    }
+ 
+    public function getItem($key)
+    {
+ 
+    }
+ 
+    public function get()
+    {
+        if(isset($this->content)) {
+            return $this->content;
+        } else {
+            return null;
+        }
+    }
+ 
+    public function set($content)
+    {
+        $this->content = $content;
+    }
+ 
+    public function save()
+    {
+ 
+    }
+ 
+    public function expiresAfter($cache_lifetime = null)
+    {
+        return true;
+    }
+ 
+}
